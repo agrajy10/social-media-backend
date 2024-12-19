4ef4841459308c1661f916fa5be2eade87472ec7
Agraj Yadav <agrajy10@gmail.com>
2024-12-19 23:28:10 +0530

    implement api endpoint for user login

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import e from "express";
 import helmet from "helmet";
 import { body, validationResult } from "express-validator";
 import encryptPassword from "./utils/encrptPassword.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const app = e();
 const prisma = new PrismaClient();
@@ -47,6 +49,11 @@ const userRegisterValidator = [
   body("name").notEmpty().withMessage("Name is required").escape(),
 ];
 
+const userLoginValidator = [
+  body("username").notEmpty().withMessage("Username is required").escape(),
+  body("password").notEmpty().withMessage("Password is required").escape(),
+];
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
@@ -68,6 +75,61 @@ app.post("/user/register", userRegisterValidator, async (req, res) => {
         name: req.body.name,
         password: hashedPassword,
       },
+    });
+
+    return res.json({
+      email: user.email,
+      username: user.username,
+      name: user.name,
+      profileImage: user.profileImage,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/user/login", userLoginValidator, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array().map((error) => ({ [error.path]: error.msg })),
+    });
+  }
+
+  const { username, password } = req.body;
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        OR: [
+          {
+            email: username,
+          },
+          {
+            username: username,
+          },
+        ],
+      },
+    });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user.id, username: user.username },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 24 * 60 * 60 * 1000,
     });
 
     return res.json({

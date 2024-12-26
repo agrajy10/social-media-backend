b85fa65af27b27341a1f3c43c8bdc24e773e30e5
Agraj Yadav <agrajy10@gmail.com>
2024-12-26 19:39:54 +0530

    update structure of error response

--- middleware/handleValidation.js
+++ middleware/handleValidation.js
@@ -4,13 +4,19 @@ function handleValidation(req, res, next) {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({
+      status: "error",
+      message: "Validation failed",
       errors: errors.array().map((error) => {
         if (error.type === "alternative_grouped") {
           return error.nestedErrors.flat().map((nestedError) => ({
-            [nestedError.path]: nestedError.msg,
+            field: nestedError.path,
+            message: nestedError.msg,
           }));
         }
-        return { [error.path]: error.msg };
+        return {
+          field: error.path,
+          message: error.msg,
+        };
       }),
     });
   }

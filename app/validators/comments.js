b091934fffda9bf8de1473e93f5def0a9a25964f
Agraj Yadav <agrajy10@gmail.com>
2025-01-14 23:01:21 +0530

    refactor validators

--- app/validators/comments.js
+++ app/validators/comments.js
@@ -1,7 +1,7 @@
 import { body, param } from "express-validator";
 import { prisma } from "../index.js";
 
-export const createCommentValidator = [
+const postIdValidator = [
   param("postId")
     .notEmpty()
     .withMessage("Post ID is required")
@@ -18,76 +18,40 @@ export const createCommentValidator = [
           }
         });
     }),
+];
+
+const commentIdValidator = [
+  param("commentId")
+    .notEmpty()
+    .withMessage("Comment ID is required")
+    .bail()
+    .isNumeric()
+    .withMessage("Invalid Comment ID")
+    .bail()
+    .custom((value) => {
+      return prisma.comment
+        .findUnique({ where: { id: parseInt(value) } })
+        .then((comment) => {
+          if (!comment) {
+            return Promise.reject("No comment with this id exists");
+          }
+        });
+    }),
+];
+
+const commentContentValidator = [
   body("content").notEmpty().withMessage("Content is required"),
+];
+
+export const createCommentValidator = [
+  ...postIdValidator,
+  ...commentContentValidator,
 ];
 
 export const commentReplyValidator = [
-  param("postId")
-    .notEmpty()
-    .withMessage("Post ID is required")
-    .bail()
-    .isNumeric()
-    .withMessage("Invalid Post ID")
-    .bail()
-    .custom((value) => {
-      return prisma.post
-        .findUnique({ where: { id: parseInt(value) } })
-        .then((post) => {
-          if (!post) {
-            return Promise.reject("No post with this id exists");
-          }
-        });
-    }),
-  param("commentId")
-    .notEmpty()
-    .withMessage("Comment ID is required")
-    .bail()
-    .isNumeric()
-    .withMessage("Invalid Comment ID")
-    .bail()
-    .custom((value) => {
-      return prisma.comment
-        .findUnique({ where: { id: parseInt(value) } })
-        .then((comment) => {
-          if (!comment) {
-            return Promise.reject("No comment with this id exists");
-          }
-        });
-    }),
-  body("content").notEmpty().withMessage("Content is required"),
+  ...postIdValidator,
+  ...commentIdValidator,
+  ...commentContentValidator,
 ];
 
-export const repliesValidator = [
-  param("postId")
-    .notEmpty()
-    .withMessage("Post ID is required")
-    .bail()
-    .isNumeric()
-    .withMessage("Invalid Post ID")
-    .bail()
-    .custom((value) => {
-      return prisma.post
-        .findUnique({ where: { id: parseInt(value) } })
-        .then((post) => {
-          if (!post) {
-            return Promise.reject("No post with this id exists");
-          }
-        });
-    }),
-  param("commentId")
-    .notEmpty()
-    .withMessage("Comment ID is required")
-    .bail()
-    .isNumeric()
-    .withMessage("Invalid Comment ID")
-    .bail()
-    .custom((value) => {
-      return prisma.comment
-        .findUnique({ where: { id: parseInt(value) } })
-        .then((comment) => {
-          if (!comment) {
-            return Promise.reject("No comment with this id exists");
-          }
-        });
-    }),
-];
+export const repliesValidator = [...postIdValidator, ...commentIdValidator];

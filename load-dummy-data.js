384a2eeff43b04d1f08c55f6f257ccabfc857469
Agraj Yadav <agrajy10@gmail.com>
2025-01-18 12:53:04 +0530

    use faker to load dummy data

---
+++ load-dummy-data.js
@@ -0,0 +1,65 @@
+import { faker } from "@faker-js/faker";
+import { prisma } from "./app/index.js";
+import encryptPassword from "./utils/encrptPassword.js";
+
+async function generateUsers() {
+  const users = [];
+
+  for (let i = 0; i < 30; i++) {
+    const hashedPassword = await encryptPassword(process.env.DEFAULT_PASSWORD);
+    const user = {
+      email: faker.internet.email(),
+      username: faker.internet.username(),
+      name: faker.person.fullName(),
+      password: hashedPassword,
+    };
+    users.push(user);
+  }
+
+  return users;
+}
+
+async function generatePosts(numberOfPosts) {
+  const posts = [];
+
+  const usersId = await prisma.user.findMany({
+    select: {
+      id: true,
+    },
+  });
+
+  for (let i = 0; i < numberOfPosts; i++) {
+    const post = {
+      title: faker.lorem.sentence(),
+      content: `<p>${faker.lorem.paragraphs(
+        faker.helpers.arrayElement([1, 2, 3])
+      )}</p>`,
+      authorId: faker.helpers.arrayElement(usersId).id,
+    };
+    posts.push(post);
+  }
+
+  return posts;
+}
+
+async function loadDummyData(batchSize, numberOfPosts) {
+  try {
+    const users = await generateUsers();
+    await prisma.user.createMany({
+      data: users,
+    });
+
+    const posts = await generatePosts(numberOfPosts);
+
+    while (posts.length) {
+      const batch = posts.splice(0, batchSize);
+      await prisma.post.createMany({
+        data: batch,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+loadDummyData(20, 120);
